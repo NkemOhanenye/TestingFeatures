f64849ee2b3b791654e4271b57f9cf35e500738e
NkemOhanenye <nohanenye3527@students.mc3.edu>
2017-10-04 15:15:13 -0400

    fix

--- src/RestartButton.java
+++ src/RestartButton.java
@@ -1,15 +1,24 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: otakusenseihig
+ * Date: 10/2/17
+ * Time: 9:25 AM
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FrameColorChanger extends JFrame implements ActionListener {
+public class RestartButton extends JFrame implements ActionListener {
     private JButton button;
+    private JButton exit;
     private JPanel panel;
     private int clickCount = 0;
 
-    public FrameColorChanger() {
+    public RestartButton() {
         button = new JButton("Click to change color");
+        exit = new JButton("Exit?");
         panel = new JPanel();
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,6 +28,7 @@ public class FrameColorChanger extends JFrame implements ActionListener {
         panel.setBackground(Color.pink);
         add(panel);
         button.addActionListener(this);
+        exit.addActionListener(this);
 
         setVisible(true);
     }
@@ -39,10 +49,36 @@ public class FrameColorChanger extends JFrame implements ActionListener {
                 button.setText("RestartButton");
             }
             if (clickCount == 6) {
-                dispose();
-                new FrameColorChanger();
+                restart();
             }
         }
+        if (e.getSource() == exit){
+            System.exit(0);
+        }
+    }
+
+    public void restart(){
+        //stop(); // if necessary
+        setup(); // set everything to initial state
+        start(); // start game
+    }
+    /*public void stop(){
+        // stop any timers, threads, operations etc.
+    }*/
+    public void setup(){
+        // set to initial state
+        // something like recreate the button
+        // clear everything done
+        panel.setVisible(false);
+    }
+    public void start(){
+        // code to initiate the program
+        button = new JButton("Click to change color");
+        panel = new JPanel();
+        panel.add(button);
+        panel.setBackground(Color.pink);
+        add(panel);
+        button.addActionListener(this);
     }
 
     public static void main(String args[]) {
